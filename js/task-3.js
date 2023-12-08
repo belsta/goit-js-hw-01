cb276aeb4af91a0e052633f2414ace69e7d62bc9
Stas Belikov <s.g.belikov@gmail.com>
2023-12-09 00:04:02 +0200

    v4

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 'use strict';
 function getElementWidth(content, padding, border) {
     const boxSizing = Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
-    return (boxSizing); 
+    return boxSizing; 
     };
